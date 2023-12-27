4edffa381b99443d2d6d3dbe6c16e40cfa019fab
vadiraj-aws <vadiraj.aws@gmail.com>
2023-12-27 20:47:22 +0530

    code refactor

--- OrderService/src/main/java/com/codeninja/orderservice/service/OrderServiceImpl.java
+++ OrderService/src/main/java/com/codeninja/orderservice/service/OrderServiceImpl.java
@@ -1,10 +1,8 @@
 package com.codeninja.orderservice.service;
 
-import java.time.Instant;
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
