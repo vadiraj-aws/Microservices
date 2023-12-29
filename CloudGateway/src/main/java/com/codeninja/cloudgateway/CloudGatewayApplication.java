63d7d29d5e8d165269fd8ddb66f4a1552966e5c7
vadiraj-aws <vadiraj.aws@gmail.com>
2023-12-29 15:06:52 +0530

    added API-GATEWAY

---
+++ CloudGateway/src/main/java/com/codeninja/cloudgateway/CloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.codeninja.cloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGatewayApplication.class, args);
+	}
+
+}
